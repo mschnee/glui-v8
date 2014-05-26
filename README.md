61c946a538967d6c486477afdd41d34a3a2bd682
Matthew Schnee <matthew.schnee+github@gmail.com>
2014-05-25 22:00:40 -0700

    Some updates

--- README.md
+++ README.md
@@ -11,3 +11,17 @@ GLEW
 Qt 5.2
 
 The $QTDIR environment variable needs to be set to the location of your Qt SDK.
+
+Namespace Reference
+===================
+Core
+----
+Contains core components of the application: window management, input handling, configuration.
+
+ScriptUi
+--------
+Script components.  This glues the script context to Ui elements.
+
+Ui
+--
+Ui components.  Rects and animations and layouts and things.
--- src/Ui/Rect.h
+++ src/Ui/Rect.h
@@ -1,6 +1,35 @@
 #pragma once
 namespace Ui {
-  class Rect {
+    enum HorizontalAlignment {
+        ALIGN_LEFT, ALIGN_CENTER, ALIGN_RIGHT
+    };
+    enum VerticalAlignment {
+        ALIGN_TOP, ALIGN_MIDDLE, ALIGN_BOTTOM
+    };
     
-  };
+    typedef std::shared_ptr<Rect> RectPtr;
+    
+    /**
+     * Base class for all things Rectangular in the UI.
+     */
+    class Rect {
+        
+        public:
+            void setParent(RectPtr);
+        private:
+            RectPtr parent;
+            std::list<RectPtr> children;
+            
+            // x, y, w, h
+            float position[4];
+            
+            // top, right, bottom, left
+            float margin[4];
+            float padding[4];
+            float border[4];
+            
+            HorizontalAlignment hAlign, parentHAlign;
+            VerticalAlignment vAlign, parentVAlign;
+            
+    };
 }
